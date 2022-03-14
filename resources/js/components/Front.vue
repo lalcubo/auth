26d37daa4270a1b039926ada672e718c5d550b15
Luis Sanchez <lalcubo@gmail.com>
2022-03-13 22:52:02 -0400

    se cambio el nombre usuario ejemplo en front

--- resources/js/components/Front.vue
+++ resources/js/components/Front.vue
@@ -8,7 +8,7 @@
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title>John Leider</v-list-item-title>
+            <v-list-item-title>Kenny contreras</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
